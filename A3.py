eeb5ccd785b263cb9e1d9b9c5966726ae38407d4
sutarsmera-debug <sutarsmera@gmail.com>
2026-07-13 16:45:53 +0530

    Add files via upload

---
+++ A3.py
@@ -0,0 +1,7 @@
+def square(number):
+    return number*number
+
+num = int(input("Enter a number: "))
+
+result = square(num)
+print(f"The square of {num} is {result}")
---
+++ A4.py
@@ -0,0 +1,11 @@
+def  factorial(x):
+    if x==0 or x==1:
+        return 1
+    else:
+        return x*factorial(x-1)
+    
+print("factorial of 0: ",factorial(0))
+print("factorial of 1:",factorial(1))
+print("factorial of 2: ",factorial(2))
+print("factorial of 5: ",factorial(5))
+print("factorial of 10: ",factorial(10))
